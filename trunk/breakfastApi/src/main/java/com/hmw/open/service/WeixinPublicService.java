3b610b2943def32206ca7714abb00e4392570aa5
zhangjinfeng <1412619418@qq.com>
2020-07-09 13:32:43 +0800

    删除

--- trunk/breakfastApi/src/main/java/com/hmw/open/service/WeixinPublicService.java
+++
@@ -1,9 +0,0 @@
-package com.hmw.open.service;
-
-import java.util.TreeMap;
-
-public interface WeixinPublicService {
-	
-	public void sendTemplate(TreeMap<String,TreeMap<String,String>> params,String templateName,String sessionId) ;
-	public void sendTemplate2(TreeMap<String,TreeMap<String,String>> params,String templateName,String sessionId,String reUrl) ;
-}
